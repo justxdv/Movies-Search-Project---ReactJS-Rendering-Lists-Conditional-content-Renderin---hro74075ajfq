68d74f82b6ad759cc48e68c3bfe4b247c952bca7
Digvijay Sethi <59555227+justxdv@users.noreply.github.com>
2023-03-04 16:19:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,34 +3,46 @@ import "../styles/App.css";
 import { movies } from "../utils/movieList";
 
 const App = () => {
-    const [inputValue, setInputValue] = useState("");
-    const [results, setResult] = useState(null);
+  const [inputValue, setInputValue] = useState("");
+  const [results, setResults] = useState([]);
 
-    function handleClick(e) {}
+  function handleClick(e) {
+    e.preventDefault();
 
-    function handleChange(e) {}
-
-    return (
-        <div id="main">
-            <form id="form">
-                <input
-                    type="text"
-                    onChange={handleChange}
-                    value={inputValue}
-                    id="movie-input"
-                ></input>
-                <button id="button" onClick={handleClick}>
-                    Search
-                </button>
-            </form>
-            <div id="result">
-                {results &&
-                    results.map((movie) => (
-                        <div className="movie">{movie}</div>
-                    ))}
-            </div>
-        </div>
+    // Search for movies that match the input value (case insensitive)
+    const matchingMovies = movies.filter((movie) =>
+      movie.title.toLowerCase().includes(inputValue.toLowerCase())
     );
+
+    setResults(matchingMovies);
+  }
+
+  function handleChange(e) {
+    setInputValue(e.target.value);
+  }
+
+  return (
+    <div id="main">
+      <form id="form">
+        <input
+          type="text"
+          onChange={handleChange}
+          value={inputValue}
+          id="movie-input"
+        ></input>
+        <button id="button" onClick={handleClick}>
+          Search
+        </button>
+      </form>
+      <div id="result">
+        {results.length > 0 ? (
+          results.map((movie) => <div className="movie">{movie.title}</div>)
+        ) : (
+          <div>No matching movies found</div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default App;
